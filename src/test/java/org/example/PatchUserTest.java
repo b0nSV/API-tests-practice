fa1e3204e69660127efd47f9325ffbc5f460dcd5
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-07-31 01:22:52 +0300

    Add negative tests for path user request

--- src/test/java/org/example/PatchUserTest.java
+++ src/test/java/org/example/PatchUserTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.buiseness_entities.ErrorMessageResponse;
 import org.example.buiseness_entities.PatchUserResponse;
 import org.example.buiseness_entities.User;
 import io.qameta.allure.Feature;
@@ -19,6 +20,7 @@ public class PatchUserTest {
 
     static User randomUser = new User(getRandomEmail(), createRandomPassword(8), getRandomName());
     static String accessToken;
+    static String anotherUserAccessToken;
 
     @BeforeClass
     public static void registerRandomUser() {
@@ -26,21 +28,21 @@ public class PatchUserTest {
     }
 
     @Test
-    public void patchUserWithTokenReturnsStatus200Test(){
+    public void patchUserWithTokenReturnsStatus200Test() {
         var newUserData = new User("new" + randomUser.getEmail(), null, null);
         Response response = patchUser(newUserData, accessToken);
-        assertEquals(SC_OK, response.statusCode());
+        assertEquals(SC_OK, response.getStatusCode());
     }
 
     @Test
-    public void patchUserWithTokenReturnsSuccessTrueTest(){
+    public void patchUserWithTokenReturnsSuccessTrueTest() {
         var newUserData = new User("new" + randomUser.getEmail(), null, null);
         var patchUserResponse = patchUser(newUserData, accessToken).as(PatchUserResponse.class);
         assertTrue(patchUserResponse.isSuccess());
     }
 
     @Test
-    public void patchUserEmailReturnsNewEmailTest(){
+    public void patchUserEmailReturnsNewEmailTest() {
         var newEmail = "new" + randomUser.getEmail();
         var newUserData = new User(newEmail, null, null);
         var patchUserResponse = patchUser(newUserData, accessToken).as(PatchUserResponse.class);
@@ -48,7 +50,7 @@ public class PatchUserTest {
     }
 
     @Test
-    public void patchUserNameReturnsNewNameTest(){
+    public void patchUserNameReturnsNewNameTest() {
         var newName = "новый" + randomUser.getName();
         var newUserData = new User(null, null, newName);
         var patchUserResponse = patchUser(newUserData, accessToken).as(PatchUserResponse.class);
@@ -56,15 +58,52 @@ public class PatchUserTest {
     }
 
     @Test
-    public void patchUserPasswordDoNotReturnsPasswordTest(){
+    public void patchUserPasswordDoNotReturnsPasswordTest() {
         var newPassword = createRandomPassword(9);
         var newUserData = new User(null, newPassword, null);
         var patchUserResponse = patchUser(newUserData, accessToken).as(PatchUserResponse.class);
         assertNull(patchUserResponse.getUser().getPassword());
     }
 
+    @Test
+    public void patchUserWithoutTokenReturnsStatus401Test() {
+        var newPassword = createRandomPassword(9);
+        var newUserData = new User(null, newPassword, null);
+        var patchUserResponse = patchUser(newUserData);
+        assertEquals(SC_UNAUTHORIZED, patchUserResponse.getStatusCode());
+    }
+
+    @Test
+    public void patchUserWithoutTokenReturnsErrorMessageTest() {
+        var newPassword = createRandomPassword(9);
+        var newUserData = new User(null, newPassword, null);
+        var patchUserResponse = patchUser(newUserData).as(ErrorMessageResponse.class);
+        assertEquals("You should be authorised", patchUserResponse.getMessage());
+    }
+
+    @Test
+    public void patchUserUsingAnotherUserEmailReturns403Test() {
+        var anotherRandomUser = new User(getRandomEmail(), createRandomPassword(8), getRandomName());
+        anotherUserAccessToken = registerUser(anotherRandomUser).getAuthToken();
+        var patchUserResponse = patchUser(new User(anotherRandomUser.getEmail(), null, null), accessToken);
+        assertEquals(SC_FORBIDDEN, patchUserResponse.getStatusCode());
+    }
+
+    @Test
+    public void patchUserUsingAnotherUserEmailReturnsErrorMessageTest() {
+        var anotherRandomUser = new User(getRandomEmail(), createRandomPassword(8), getRandomName());
+        anotherUserAccessToken = registerUser(anotherRandomUser).getAuthToken();
+        var patchUserResponse = patchUser(new User(anotherRandomUser.getEmail(), null, null), accessToken);
+        assertEquals("User with such email already exists", patchUserResponse.as(ErrorMessageResponse.class).getMessage());
+    }
+
+    @After
+    public void deleteAnotherUserAfterTest() {
+        if (anotherUserAccessToken != null) deleteUser(anotherUserAccessToken);
+    }
+
     @AfterClass
-    public static void deleteUserAfterTest(){
+    public static void deleteUserAfterTests() {
         if (accessToken != null) deleteUser(accessToken);
     }
 
