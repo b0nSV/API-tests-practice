e2ec4f3a2f4b1f1da91d62b9f0002b16055feb9c
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-07-31 22:38:53 +0300

    Add getUserOrders tests constant name

--- src/main/java/org/example/helpers/entities/TestsByUrlName.java
+++ src/main/java/org/example/helpers/entities/TestsByUrlName.java
@@ -1,5 +1,6 @@
 package org.example.helpers.entities;
 
+import static org.example.steps.OrderSteps.GET_ORDERS_URL;
 import static org.example.steps.UserSteps.LOGIN_COURIER_URL;
 import static org.example.steps.UserSteps.REGISTER_COURIER_URL;
 import static org.example.steps.UserSteps.PATCH_COURIER_URL;
@@ -10,4 +11,5 @@ public interface TestsByUrlName {
     String REGISTER_USER_METHOD_TESTS_NAME = "Регистрация пользователя - POST " + REGISTER_COURIER_URL;
     String PARTIAL_UPDATE_USER_METHOD_TESTS_NAME = "Частичное обновление данных пользователя - PATCH " + PATCH_COURIER_URL;
     String CREATE_ORDER_METHOD_TESTS_NAME = "Создание заказа - POST " + CREATE_ORDER_URL;
+    String GET_USER_ORDERS_METHOD_TESTS_NAME = "Получить заказы конкретного пользователя - GET " + GET_ORDERS_URL;
 }
