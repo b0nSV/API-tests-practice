3e75a7c2e986c0b94657e37c771cf861ef6e5a90
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-07-31 18:43:37 +0300

    Add tests

--- src/test/java/org/example/CreateOrderTest.java
+++ src/test/java/org/example/CreateOrderTest.java
@@ -1,16 +1,18 @@
 package org.example;
 
 import io.qameta.allure.Feature;
+import org.example.buiseness_entities.ErrorMessageResponse;
 import org.example.buiseness_entities.OrderCreate;
+import org.example.buiseness_entities.OrderCreateResponse;
 import org.example.buiseness_entities.User;
 import org.junit.*;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.apache.http.HttpStatus.SC_OK;
-import static org.example.steps.IngredientSteps.*;
+import static org.apache.http.HttpStatus.*;
 import static org.junit.Assert.*;
+import static org.example.steps.IngredientSteps.*;
 import static org.example.helpers.RandomSequences.*;
 import static org.example.helpers.entities.TestsByUrlName.CREATE_ORDER_METHOD_TESTS_NAME;
 import static org.example.helpers.entities.IngredientTypes.*;
@@ -23,7 +25,6 @@ import static org.example.steps.OrderSteps.createOrder;
 public class CreateOrderTest {
 
     static String accessToken;
-//    static HashMap<String, ArrayList<String>> ingredientsHashesByType;
     static Map<String, List<String>> ingredientIdsPerType;
     OrderCreate order;
 
@@ -34,23 +35,65 @@ public class CreateOrderTest {
         ingredientIdsPerType = getIngredientIdsPerType();
     }
 
-/*    @Before
-    public void createRandomCourierAndGetAccessToken() {
-
-    }*/
-
     @Test
     public void createOrderWithoutAuthTokenReturnsStatus200Test() {
-        order = new OrderCreate(List.of((ingredientIdsPerType.get(TYPE_BUN).get(0))));
+        order = new OrderCreate(List.of(
+                (ingredientIdsPerType.get(TYPE_BUN).get(0)),
+                (ingredientIdsPerType.get(TYPE_MAIN).get(0)),
+                (ingredientIdsPerType.get(TYPE_SAUCE).get(0))
+        ));
         var response = createOrder(order);
         assertEquals(SC_OK, response.getStatusCode());
     }
 
     @Test
     public void createOrderWithAuthTokenReturnsStatus200Test() {
-        order = new OrderCreate(List.of((ingredientIdsPerType.get(TYPE_BUN).get(0))));
+        order = new OrderCreate(List.of(
+                (ingredientIdsPerType.get(TYPE_BUN).get(0)),
+                (ingredientIdsPerType.get(TYPE_MAIN).get(0)),
+                (ingredientIdsPerType.get(TYPE_SAUCE).get(0))
+        ));
         var response = createOrder(order, accessToken);
         assertEquals(SC_OK, response.getStatusCode());
+    }
+
+    @Test
+    public void createOrderWithAuthTokenReturnsOrderNumberTest() {
+        order = new OrderCreate(List.of(
+                (ingredientIdsPerType.get(TYPE_BUN).get(0)),
+                (ingredientIdsPerType.get(TYPE_MAIN).get(0)),
+                (ingredientIdsPerType.get(TYPE_SAUCE).get(0))
+        ));
+        var response = createOrder(order, accessToken);
+        assertNotEquals(0, response.as(OrderCreateResponse.class).getOrder().getNumber());
+    }
+
+    @Test
+    public void createOrderWithWrongIngredientHashReturnsStatus500Test() {
+        order = new OrderCreate(List.of("a" + (ingredientIdsPerType.get(TYPE_BUN).get(0).substring(1))));
+        var response = createOrder(order, accessToken);
+        assertEquals(SC_INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
+    public void createOrderWithoutIngredientsReturnsStatus400Test() {
+        order = new OrderCreate(List.of());
+        var response = createOrder(order, accessToken);
+        assertEquals(SC_BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void createOrderWithoutIngredientsReturnsErrorMessageTest() {
+        order = new OrderCreate(List.of());
+        var response = createOrder(order, accessToken);
+        assertEquals("Ingredient ids must be provided", response.as(ErrorMessageResponse.class).getMessage());
+    }
+
+    @Test
+    public void createOrderWithoutIngredientsReturnsSuccessFalseTest() {
+        order = new OrderCreate(List.of());
+        var response = createOrder(order, accessToken);
+        assertFalse(response.as(ErrorMessageResponse.class).isSuccess());
     }
 
     @AfterClass
